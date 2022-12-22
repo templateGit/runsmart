f03f947dc98845d3b08be713ae3524780588ab2e
templateGit <107248571+templateGit@users.noreply.github.com>
2022-12-23 00:49:50 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,162 @@
+const slider = tns({
+   container: '.carousel__inner',
+   items: 1,
+   speed: 700,
+   slideBy: 'page',
+   autoplay: false,
+   controls: false,
+   navPosition: "bottom",
+   responsive: {
+      1200: {
+         items: 1,
+         autowidth: true,
+      },
+   }
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+   slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+   slider.goTo('next');
+});
+
+
+(function ($) {
+   $(function () {
+
+      $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
+         $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+      });
+
+      function toggleSlide(item) {
+         $(item).each(function (i) {
+            $(this).on('click', function (e) {
+               e.preventDefault();
+               $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+               $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            })
+         });
+      };
+
+      toggleSlide('.catalog-item__link');
+      toggleSlide('.catalog-item__back');
+
+
+      //Modal
+
+      $('[data-modal=consultation]').on('click', function () {
+         $('.overlay, #consultation').fadeIn();
+      });
+
+      $('.modal__close').on('click', function () {
+         $('.overlay, #consultation, #order, #thanks').fadeOut();
+      });
+
+      $('.button_catalog').each(function (i) {
+         $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $('.overlay, #order').fadeIn();
+         })
+      });
+
+
+      function validateForms(form) {
+         $(form).validate({
+            rules: {
+               name: {
+                  required: true,
+                  minlength: 2
+               },
+               tel: {
+                  required: true,
+                  minlength: 11
+               },
+               email: {
+                  required: true,
+                  email: true
+               }
+            },
+            messages: {
+               name: {
+                  required: "Введите имя",
+                  minlength: jQuery.validator.format("Введите {0} символов")
+               },
+               tel: {
+                  required: "Введите номер телефона",
+                  minlength: jQuery.validator.format("Введите {0} символов")
+               },
+               email: {
+                  required: "Введите Е-mail",
+                  email: "Формат почты долден быть таким: name@yandex.ru"
+               }
+            }
+         });
+      };
+
+      validateForms('#order form');
+      validateForms('#consultation-form');
+      validateForms('#consultation form');
+
+
+      $('input[name=tel]').mask("+375 99 999-99-99");
+
+
+      $('form').submit(function (e) {
+         e.preventDefault();
+
+         if (!$(this).valid()) {
+            return;
+         }
+
+         $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+         }).done(function () {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn();
+
+            $('form').trigger('reset');
+         });
+         return false;
+      });
+
+
+      //Scroll
+
+      $(window).scroll(function () {
+         if ($(this).scrollTop() > 1123) {
+            $('.pageup').fadeIn();
+         } else {
+            $('.pageup').fadeOut();
+         }
+      });
+
+
+      //Smooth Scrolling
+
+      $("a[href='#promo']").click(function () {
+         const _href = $(this).attr("href");
+         $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+         return false;
+      });
+
+      $("a[href='#catalog']").click(function () {
+         const _href = $(this).attr("href");
+         $("html, body").animate({ scrollTop: $(_href).offset().top + "px" });
+         return false;
+      });
+
+
+      //Animation
+
+      new WOW().init();
+
+
+   });
+})(jQuery);
